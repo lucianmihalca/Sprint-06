a4a9ffea7e4f2337467bdd8ea2efe9cc1ee4b53a
LucianMihalca <infolucianmihalca@gmail.com>
2024-02-05 17:53:33 +0100

    Instalar dependencia axios para manejar peticiones al backend, refactorizar servicios: players, games, ranking para usar axios en vez de fetch

--- frontend/src/services/games.services.ts
+++ frontend/src/services/games.services.ts
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 const BASE_URL = "http://localhost:3000";
 
 /**
@@ -5,47 +7,50 @@ const BASE_URL = "http://localhost:3000";
  * Realiza una solicitud POST al servidor para generar un nuevo juego basado
  * en el ID del jugador proporcionado como parámetro. El servidor responderá
  * con los datos del juego creado, incluyendo el resultado de los dados y si se ganó o no el juego.
- * 
+ *
  * @param {string} id - El ID del jugador para el cual se creará el juego.
  * @returns {Promise<Object>} - Promesa que resuelve con los datos del juego creado.
  */
 async function playGame(id: string) {
   console.log(`Intentando crear juego para el jugador con ID: ${id}`);
-  const response = await fetch(`${BASE_URL}/games/${id}`, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" }
-  });
-
-  if (!response.ok) {
-    const errorResponse = await response.text();
-    console.error(`Error al crear el juego: ${errorResponse}`);
-    throw new Error("No se pudo crear el juego");
+  try {
+    const response = await axios.post(`${BASE_URL}/games/${id}`);
+    console.log("Juego creado con éxito:", response.data);
+    return response.data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      const message = `Error al crear el juego para el jugador con ID: ${id}`;
+      console.error(message);
+      throw new Error(message);
+    } else {
+      console.error(`Error inesperado al crear el juego: ${error}`);
+      throw new Error("Error inesperado al crear el juego.");
+    }
   }
-
-  const gameData = await response.json();
-  console.log("Juego creado con éxito:", gameData);
-  return gameData;
 }
 
 /**
  * Obtiene todos los juegos realizados por un jugador específico a partir de su ID.
  * Realiza una solicitud GET al servidor para recuperar una lista de todos los juegos
  * jugados por el jugador, incluyendo los resultados de cada uno.
- * 
+ *
  * @param {string} id - El ID del jugador cuyos juegos se quieren obtener.
  * @returns {Promise<Array>} - Promesa que resuelve con la lista de juegos del jugador.
  */
 async function getGamesByPlayerId(id: string) {
-  const response = await fetch(`${BASE_URL}/games/${id}`, {
-    method: "GET",
-    headers: { "Content-Type": "application/json" }
-  });
-
-  if (!response.ok) {
-    throw new Error(`HTTP error! status: ${response.status}`);
+  try {
+    const response = await axios.get(`${BASE_URL}/games/${id}`);
+    return response.data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      const message = `Error al obtener los juegos del jugador con ID: ${id}`;
+      console.error(message);
+      throw new Error(message);
+    } else {
+      console.error(`Error inesperado al obtener los juegos: ${error}`);
+      throw new Error("Error inesperado al obtener los juegos.");
+    }
   }
-
-  return await response.json();
 }
 
 /**
@@ -53,21 +58,24 @@ async function getGamesByPlayerId(id: string) {
  * Realiza una solicitud DELETE al servidor para eliminar todos los juegos
  * previamente jugados por el jugador. Esta operación no devuelve datos del juego,
  * pero puede responder con una confirmación de la eliminación.
- * 
+ *
  * @param {string} id - El ID del jugador cuyos juegos se desean eliminar.
  * @returns {Promise<Object>} - Promesa que resuelve con la confirmación de la eliminación.
  */
 async function deleteGamesByPlayerId(id: string) {
-  const response = await fetch(`${BASE_URL}/games/${id}`, {
-    method: "DELETE",
-    headers: { "Content-Type": "application/json" }
-  });
-
-  if (!response.ok) {
-    throw new Error(`HTTP error! status: ${response.status}`);
+  try {
+    const response = await axios.delete(`${BASE_URL}/games/${id}`);
+    return response.data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      const message = `Error al eliminar los juegos del jugador con ID: ${id}`;
+      console.error(message);
+      throw new Error(message);
+    } else {
+      console.error(`Error inesperado al eliminar los juegos: ${error}`);
+      throw new Error("Error inesperado al eliminar los juegos.");
+    }
   }
-
-  return await response.json(); 
 }
 
 export const gameService = {
--- frontend/src/services/ranking.services.ts
+++ frontend/src/services/ranking.services.ts
@@ -1,68 +1,77 @@
+import axios from "axios";
 const BASE_URL = "http://localhost:3000";
 
 /**
  * Obtiene el ranking de todos los jugadores junto con el porcentaje de éxito medio.
  * Realiza una solicitud GET al servidor para recuperar el ranking ordenado
  * y el porcentaje de éxito medio de todos los jugadores.
- * 
+ *
  * @returns {Promise<Object>} Promesa que resuelve con los datos del ranking y el promedio de éxito.
  */
 async function getRanking() {
-  const response = await fetch(`${BASE_URL}/ranking`, {
-    method: "GET",
-    headers: { "Content-Type": "application/json" },
-  });
-
-  if (!response.ok) {
-    throw new Error(`HTTP error! status: ${response.status}`);
+  try {
+    const response = await axios.get(`${BASE_URL}/ranking`);
+    return response.data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      const message = `Error al obtener el ranking de jugadores`;
+      console.error(message);
+      throw new Error(message);
+    } else {
+      console.error(`Error inesperado al obtener el ranking de jugadores: ${error}`);
+      throw new Error("Error inesperado al obtener el ranking de jugadores.");
+    }
   }
-
-  return await response.json();
 }
 
 /**
  * Obtiene el jugador con el peor porcentaje de éxito.
  * Realiza una solicitud GET al servidor para recuperar los detalles
  * del jugador con el peor porcentaje de éxito.
- * 
+ *
  * @returns {Promise<Object>} Promesa que resuelve con los datos del jugador con el peor porcentaje de éxito.
  */
 async function getLoser() {
-  const response = await fetch(`${BASE_URL}/ranking/loser`, {
-    method: "GET",
-    headers: { "Content-Type": "application/json" },
-  });
-
-  if (!response.ok) {
-    throw new Error(`HTTP error! status: ${response.status}`);
+  try {
+    const response = await axios.get(`${BASE_URL}/ranking/loser`);
+    return response.data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      const message = `Error al obtener el jugador con el peor porcentaje de éxito`;
+      console.error(message);
+      throw new Error(message);
+    } else {
+      console.error(`Error inesperado al obtener el jugador con el peor porcentaje de éxito: ${error}`);
+      throw new Error("Error inesperado al obtener el jugador con el peor porcentaje de éxito.");
+    }
   }
-
-  return await response.json();
 }
 
 /**
  * Obtiene el jugador con el mejor porcentaje de éxito.
  * Realiza una solicitud GET al servidor para recuperar los detalles
  * del jugador con el mejor porcentaje de éxito.
- * 
+ *
  * @returns {Promise<Object>} Promesa que resuelve con los datos del jugador con el mejor porcentaje de éxito.
  */
 async function getWinner() {
-  const response = await fetch(`${BASE_URL}/ranking/winner`, {
-    method: "GET",
-    headers: { "Content-Type": "application/json" },
-  });
-
-  if (!response.ok) {
-    throw new Error(`HTTP error! status: ${response.status}`);
+  try {
+    const response = await axios.get(`${BASE_URL}/ranking/winner`);
+    return response.data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      const message = `Error al obtener el jugador con el mejor porcentaje de éxito`;
+      console.error(message);
+      throw new Error(message);
+    } else {
+      console.error(`Error inesperado al obtener el jugador con el mejor porcentaje de éxito: ${error}`);
+      throw new Error("Error inesperado al obtener el jugador con el mejor porcentaje de éxito.");
+    }
   }
-
-  return await response.json();
 }
 
-// Exporta las funciones para su uso en otras partes de la aplicación.
 export const rankingService = {
   getRanking,
   getLoser,
-  getWinner,
+  getWinner
 };
